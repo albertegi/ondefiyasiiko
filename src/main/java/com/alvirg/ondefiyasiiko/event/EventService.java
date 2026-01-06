61be7cd3a9563adbf3608843e63c85f51737a8d6
albertegi <albertegi90@gmail.com>
2026-01-06 11:54:14 +0000

    Fix in EventService proper importation of EventRequest from the right package

--- src/main/java/com/alvirg/ondefiyasiiko/event/EventService.java
+++ src/main/java/com/alvirg/ondefiyasiiko/event/EventService.java
@@ -1,12 +1,12 @@
 package com.alvirg.ondefiyasiiko.event;
 
+import com.alvirg.ondefiyasiiko.event.request.EventRequest;
 import com.alvirg.ondefiyasiiko.event.response.EventResponse;
-import com.sun.jdi.request.EventRequest;
 
 import java.util.List;
 
 public interface EventService {
-    String createEvent(EventRequest request, String userId);
+    String createEvent(EventRequest request);
     List<EventResponse> getAllEvents();
 
 
