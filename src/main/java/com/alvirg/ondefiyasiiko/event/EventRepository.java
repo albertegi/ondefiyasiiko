04c0168e82fab6921f704017fb6ed2fdf2dc3046
albertegi <albertegi90@gmail.com>
2026-01-06 11:55:56 +0000

    Fix in EventRepository ** used JPA supplied derived query findById

--- src/main/java/com/alvirg/ondefiyasiiko/event/EventRepository.java
+++ src/main/java/com/alvirg/ondefiyasiiko/event/EventRepository.java
@@ -1,9 +1,20 @@
 package com.alvirg.ondefiyasiiko.event;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface EventRepository extends JpaRepository<Event, String> {
     List<Event> findAllByOrderByStartTimeAsc();
+
+
+//    @Query("""
+//            SELECT e
+//            FROM Event e
+//            WHERE e.id = :id
+//            """)
+//    Optional<Event> findEventById(@Param("id") String id);
 }
