b378a8afcb096ae86407d3b26716f2cb287368cf
albertegi <albertegi90@gmail.com>
2026-01-08 20:27:09 +0000

    Add AnnouncementService and the various abstract methods as part of the contract to be implemented.

---
+++ src/main/java/com/alvirg/ondefiyasiiko/announcement/AnnouncementService.java
@@ -0,0 +1,15 @@
+package com.alvirg.ondefiyasiiko.announcement;
+
+import com.alvirg.ondefiyasiiko.event.request.EventRequest;
+import com.alvirg.ondefiyasiiko.event.request.EventUpdateRequest;
+import com.alvirg.ondefiyasiiko.event.response.EventResponse;
+
+import java.util.List;
+
+public interface AnnouncementService {
+    String createAnnouncement(AnnouncementRequest request);
+    void updateAnnouncement(AnnouncementUpdateRequest request, String userId);
+    AnnouncementResponse getAnnouncementById(String announcementId);
+    List<AnnouncementResponse> getAllAnnouncement();
+    void deleteAnnouncement(String AnnouncementId);
+}
