45017dca82797c7080f81e7847cb95f5fbf50cee
albertegi <albertegi90@gmail.com>
2026-01-08 20:50:20 +0000

    Add AnnouncementRepository

---
+++ src/main/java/com/alvirg/ondefiyasiiko/announcement/AnnouncementRepository.java
@@ -0,0 +1,6 @@
+package com.alvirg.ondefiyasiiko.announcement;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnnouncementRepository extends JpaRepository<Announcement, String> {
+}
