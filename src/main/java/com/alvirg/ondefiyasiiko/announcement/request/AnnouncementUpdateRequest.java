e24406edc2dedde14cb6628f47152165ea7ffcd2
albertegi <albertegi90@gmail.com>
2026-01-08 20:38:27 +0000

    Add AnnouncementUpdateRequest  ** add title, content, published_at attributes and validation messages Also add its relationship with Festival entity

---
+++ src/main/java/com/alvirg/ondefiyasiiko/announcement/request/AnnouncementUpdateRequest.java
@@ -0,0 +1,25 @@
+package com.alvirg.ondefiyasiiko.announcement.request;
+
+import com.alvirg.ondefiyasiiko.festival.Festival;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDateTime;
+
+public class AnnouncementUpdateRequest {
+
+    @NotBlank(message = "VALIDATION.ANNOUNCEMENT.TITLE.NOT_BLANK")
+    private String title;
+
+    @NotBlank(message = "VALIDATION.ANNOUNCEMENT.CONTENT.NOT_BLANK")
+    private String content;
+
+    @NotNull(message = "VALIDATION.ANNOUNCEMENT.PUBLISHED_AT.NOT_NULL")
+    @FutureOrPresent(message = "VALIDATION.ANNOUNCEMENT.PUBLISHED_AT.FUTURE_OR_PRESENT")
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    private LocalDateTime publishedAt;
+
+    private Festival festival;
+}
