ebe7c3fe7dc086a4e8b5f92decba81fcb9d1b2d1
albertegi <albertegi90@gmail.com>
2026-01-11 00:28:19 +0000

    Create PerformerRequest with all require attributes including the relationship entity it has with festival and their validation messages

---
+++ src/main/java/com/alvirg/ondefiyasiiko/performer/request/PerformerRequest.java
@@ -0,0 +1,32 @@
+package com.alvirg.ondefiyasiiko.performer.request;
+
+import com.alvirg.ondefiyasiiko.common.ApplicationStatus;
+import com.alvirg.ondefiyasiiko.festival.Festival;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+
+import java.time.LocalDateTime;
+
+public class PerformerRequest {
+
+    @NotBlank(message = "VALIDATION.PERFORMER.NAME.NOT_BLANK")
+    private String name;
+    @NotBlank(message = "VALIDATION.PERFORMER.EMAIL.NOT_BLANK")
+    private String email;
+    @NotBlank(message = "VALIDATION.PERFORMER.PHONE_NUMBER.NOT_BLANK")
+    private String phoneNumber;
+    @NotBlank(message = "VALIDATION.PERFORMER.PERFORMANCE_TYPE.NOT_BLANK")
+    private String performanceType; // MUSIC, DANCE, POETRY, OTHER
+    @NotBlank(message = "VALIDATION.PERFORMER.DESCRIPTION.NOT_BLANK")
+    private String description;
+    @NotBlank(message = "VALIDATION.PERFORMER.EQUIPMENT.NOT_BLANK")
+    private String equipment;
+    @NotBlank(message = "VALIDATION.PERFORMER.DURATION.NOT_BLANK")
+    private Integer duration; // in minutes
+    @NotBlank(message = "VALIDATION.PERFORMER.PREVIOUS_EXPERIENCE.NOT_BLANK")
+    private String previousExperience;
+
+    @Enumerated(EnumType.STRING)
+    private ApplicationStatus performerStatus = ApplicationStatus.PENDING; // PENDING, APPROVED, REJECTED
+    private Festival festival;
+}
