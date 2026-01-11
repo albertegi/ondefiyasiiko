96486156f2ccb5bee51e4745a1104c477b82f923
albertegi <albertegi90@gmail.com>
2026-01-11 00:36:48 +0000

    Create PerformerServiceImpl class that implements all the methods in PerformerService

---
+++ src/main/java/com/alvirg/ondefiyasiiko/performer/impl/PerformerServiceImpl.java
@@ -0,0 +1,23 @@
+package com.alvirg.ondefiyasiiko.performer.impl;
+
+import com.alvirg.ondefiyasiiko.performer.Performer;
+import com.alvirg.ondefiyasiiko.performer.PerformerService;
+
+import java.util.List;
+
+public class PerformerServiceImpl implements PerformerService {
+    @Override
+    public String registerPerformer(Performer performer) {
+        return "";
+    }
+
+    @Override
+    public List<Performer> getAllPerformers() {
+        return List.of();
+    }
+
+    @Override
+    public List<Performer> getPerformersByStatus(String status) {
+        return List.of();
+    }
+}
